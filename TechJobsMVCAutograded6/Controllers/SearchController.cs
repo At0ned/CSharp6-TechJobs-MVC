99f657af466a6c6f6b1a0fe9cb54a35d7a123efe
At0ned <sethaholt@gmail.com>
2024-06-07 14:27:32 -0500

    Task 3 done, might have errors

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -18,5 +18,22 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        List<Job> jobs = new();
+        if (searchType == "all" || searchType == "")
+        {
+        jobs = JobData.FindAll();
+        }
+        else 
+        {
+        jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+
+        }
+        ViewBag.columns = ListController.ColumnChoices;
+
+        return View("Index", jobs);
+    }
 }
 
